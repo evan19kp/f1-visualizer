0bdc54353b7ccea190f261004d131ada7c67bc61
evan19kp <evankpackard@gmail.com>
2026-06-04 04:17:58 -0700

    first commit

---
+++ src/main/java/com/evanp/f1_visualizer/F1VisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.evanp.f1_visualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1VisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1VisualizerApplication.class, args);
+	}
+
+}
